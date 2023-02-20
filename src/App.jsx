c8108eefc886db3156c167dfd902108da5dae29c
Mariana Navarro <maronare10@gmail.com>
2023-02-20 14:07:52 -0500

    creando boton dinámico: siguiendo y dejar de seguir y agregando array de usuarios

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,43 @@
 import './App.css'
 import TwitterFollowCard from './TwitterFollowCard'
 
+const users = [
+ {
+  userName: 'victorvzn',
+  name: 'Victor Villasón Sosa',
+  isFollowing: false,
+ },
+ {
+  userName: 'Mariana73592901',
+  name: 'Mariana Navarro Reyes',
+  isFollowing: true,
+ },
+ {
+  userName: 'cineplanet',
+  name: 'Cine Planet',
+  isFollowing: true,
+ },
+ {
+  userName: 'midudev',
+  name: 'Miguel Angel Durán',
+  isFollowing: true,
+ }
+]
+
 const App = () => {
   // const format = (userName) => `@${userName}`
   return (
     <section className='App'>
-      <TwitterFollowCard isFollowing userName="maronare">
-        Mariana Navarro
-      </TwitterFollowCard>
-
-      <TwitterFollowCard isFollowing={false} userName="leo">
-        Leo Villazón
-      </TwitterFollowCard>
-
-      <TwitterFollowCard isFollowing userName="Samy">
-        Samy Villazón
-      </TwitterFollowCard>
-
-      <TwitterFollowCard isFollowing userName="Mate">
-        Mate Villazón
-      </TwitterFollowCard>
+      {
+        users.map(({ userName, name, isFollowing }) => (
+            <TwitterFollowCard
+              key={userName}
+              userName={userName}
+              initialIsFollowing={isFollowing}>
+              {name}
+            </TwitterFollowCard>
+        ))
+      }
     </section>
   )
 }
--- src/TwitterFollowCard.jsx
+++ src/TwitterFollowCard.jsx
@@ -1,7 +1,7 @@
 import {useState} from 'react'
 
-const TwitterFollowCard = ({ children, userName }) => {
-  const [isFollowing, setIsFollowing] = useState(false)
+const TwitterFollowCard = ({ children, userName, initialIsFollowing }) => {
+  const [isFollowing, setIsFollowing] = useState(initialIsFollowing)
 
   const text = isFollowing ? 'Siguiendo' : 'Seguir'
   const buttonClassName = isFollowing
@@ -27,7 +27,8 @@ const TwitterFollowCard = ({ children, userName }) => {
 
       <aside>
         <button className={buttonClassName} onClick={handleClick}>
-          {text}
+          <span className='tw-followCard-text'>{text}</span>
+          <span className='tw-followCard-stopFollow'>Dejar de seguir</span>
         </button>
       </aside>
     </article>
